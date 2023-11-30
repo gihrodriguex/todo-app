9fb27eea6e4d6252614efe0a5376e3adb2152d09
ELIZABETH RODRIGUES DA SILVA <giovana_rodrigues@eaportal.org>
2023-11-30 05:51:41 -0300

    filtrando as tarefas ativas

--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,20 @@ function completaTarefa (id) {
         },
         body: JSON.stringify({ id })
     })
+
+    window.location.reload()
+}
+
+function descompletarTarefa(id) {
+    fetch("http://localhost:/3000/descompletar", {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify({ id })
+    })
+
+    window.location.reload()
 }
 
 function alterarTema() {
